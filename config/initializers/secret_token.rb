baac1c6842bfc13291a738989fa35112796104b1
Sanddollar <christopher.sand@gmail.com>
2012-01-02 22:16:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rant::Application.config.secret_token = '8ba2d1757cb8954dd529e84a4e91b9b2ba765711edf6dcab60b39ffd7d04b73ff87139e58c002f829744d9b5cd763378ea610af8c1a6be7cfc1b0d229f2dfaac'
